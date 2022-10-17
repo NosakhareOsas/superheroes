f5180d6903b7c3fa0a222c82fe905b731dbdd6b3
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-17 22:00:24 +0100

    hero_power actions created

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,6 @@
 class HeroPowersController < ApplicationController
+    def create
+        hero_power = HeroPower.create(strength: params[:strength], hero_id: params[:hero_id], power_id: params[:power_id])
+        render json: hero_power, status: :created
+    end
 end
