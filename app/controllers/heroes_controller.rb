ffeb5caed6f6cb92ea387a52fc0a0a1124cf9593
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-17 21:41:24 +0100

    hero actions created

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,3 +1,9 @@
 class HeroesController < ApplicationController
-    
+    def index
+        render json: Hero.all, status: :ok
+    end
+    def show
+        hero = Hero.find(params[:id])
+        render json: hero, status: :ok
+    end
 end
