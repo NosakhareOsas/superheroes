69f462b326d9577313f781a84a28d6054cb5eef7
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-17 21:52:20 +0100

    power actions created

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,14 @@
 class PowersController < ApplicationController
+    def index
+        render json: Power.all, status: :ok
+    end
+    def show
+        power = Power.find(params[:id])
+        render json: power, status: :ok
+    end
+    def update
+        power = Power.find(params[:id])
+        power.update(description: params[:description])
+        render json: power, status: :ok
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
  resources :heroes, only: [:index, :show]
  resources :powers, only: [:index, :show, :update]
  resources :hero_powers, only: [:create]
-
 end
