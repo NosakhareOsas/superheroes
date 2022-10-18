51d5197cff03d3c7467866e59d6f328186ac79d1
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-18 13:43:02 +0100

    rescue from record not found in power controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,6 @@
 class PowersController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_error
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
     def index
         render json: Power.all, status: :ok
     end
@@ -17,6 +18,10 @@ class PowersController < ApplicationController
 
     def find_power
         Power.find(params[:id])
+    end
+
+    def render_record_not_found
+        render json: {error: "Power not found"}, status: :not_found
     end
 
     def render_unprocessable_entity_error(invalid)
