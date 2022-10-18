6ad6bb397512e5b3cdb1c5b846bf366f2cf83721
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-18 13:34:03 +0100

    DRY power controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,14 +1,29 @@
 class PowersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_error
     def index
         render json: Power.all, status: :ok
     end
     def show
-        power = Power.find(params[:id])
+        power = find_power
         render json: power, status: :ok
     end
     def update
-        power = Power.find(params[:id])
-        power.update(description: params[:description])
+        power = find_power
+        power.update!(update_params)
         render json: power, status: :ok
+    end
+
+    private
+
+    def find_power
+        Power.find(params[:id])
+    end
+
+    def render_unprocessable_entity_error(invalid)
+        render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def update_params
+        params.permit(:description)
     end
 end
