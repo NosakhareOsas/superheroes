2a7f43303891037c418916303aa9fe6ec009833b
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-18 13:59:46 +0100

    rescue from record not found added to hero controller

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,9 +1,20 @@
 class HeroesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
     def index
         render json: Hero.all, status: :ok
     end
     def show
-        hero = Hero.find(params[:id])
+        hero = find_hero
         render json: hero, status: :ok
+    end
+
+    private
+
+    def find_hero
+        Hero.find(params[:id])
+    end
+
+    def render_record_not_found
+        render json: {error: "Hero not found"}, status: :not_found
     end
 end
